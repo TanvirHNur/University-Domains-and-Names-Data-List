0167d97ccaeb68938e301e9d42b60690aca775c3
TanvirHNur <tanvir.du.ier@gmail.com>
2021-09-06 13:01:59 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,80 @@
+const toggole = (id, property) => {
+    document.getElementById(id).style.display = property;
+}
+
+
+const loadUni = () => {
+    const searchInput = document.getElementById('search-text');
+    const search = searchInput.value;
+    searchInput.value ='';
+    const url = `http://universities.hipolabs.com/search?name=${search}country=${search}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayUniversity(data))
+
+    toggole('spinner', 'block')
+    toggole('uni-section', 'none')
+}
+
+const displayUniversity = universities => {
+    const container = document.getElementById('universities');
+    container.textContent = '';
+
+
+
+
+    let nothingFound = document.getElementById('nothing-found');
+        nothingFound.textContent ='';
+    if(universities.length === 0){
+        
+        const div = document.createElement("div");
+        div.classList = 'text-color';
+        div.innerHTML = `
+        <h3>Nothing found</h3> 
+        <h6>No results containing all your search terms were found.</h6> 
+      <ul>
+        Suggestions:
+        <li>Search universities by country name</li>
+        <li>Make sure that all words are spelled correctly.</li>
+        <li>Try different keywords.</li>
+        <li>
+            Try fewer keywords.
+            </li>
+      </ul>
+        `
+        nothingFound.appendChild(div)
+    }
+else{
+    
+const resultAmount = document.getElementById('results');
+
+resultAmount.innerHTML = `<h5 class="w-50 mx-auto text-color mb-5">About ${universities.length} results found</h5>`
+
+
+    universities.forEach ( university => {
+       
+
+
+        const div = document.createElement("div");
+        div.classList = 'col';
+        div.innerHTML = `
+        <div class="card h-100 university-col">
+                    
+                    <div class="card-body">
+                      <h6 class="card-title uni-color">${university.name}</h6>
+                      <p class="card-text country-color">Country: ${university.country}</p>
+                      <a class="hero-btn red-btn " id="my-btn text-center" target="_blank" href="${university.web_pages}">Visit</a>
+                    </div>
+                  </div>
+        `
+        container.appendChild(div)
+
+
+        console.log(university)
+    })
+}
+    console.log(universities);
+
+    toggole('spinner', 'none')
+    toggole('uni-section', 'block')
+}
