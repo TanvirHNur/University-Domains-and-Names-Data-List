32a9c3d87ae51c079c15d43f79f3820971527802
TanvirHNur <tanvir.du.ier@gmail.com>
2021-09-06 15:23:23 +0600

    showing 5 results by-defult

--- js/app.js
+++ js/app.js
@@ -80,11 +80,59 @@ resultAmount.innerHTML = `<h5 class="w-50 mx-auto text-color mb-5">About ${unive
         container.appendChild(div)
 
 
-        console.log(university)
+        // console.log(university)
     })
 }
-    console.log(universities);
+    // console.log(universities);
 
     toggole('spinner', 'none')
     toggole('uni-section', 'block')
 }
+
+
+// showing 5 results by-defult
+
+const loadUni1 = () => {
+    // const searchInput = document.getElementById('search-text');
+    // const search = searchInput.value;
+    // searchInput.value ='';
+    const url = `http://universities.hipolabs.com/search?country=bangladesh`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayUniversity1(data.slice(24,29)))
+
+    // toggole('spinner', 'block')
+    // toggole('uni-section', 'none')
+}
+loadUni1();
+
+// const search = document.getElementById('search-text');
+// search.addEventListener('keypress', function(even       t) {
+//     if (event.keyCode === 13) {
+//      event.preventDefault();
+//      loadUni()
+//     }
+//   });
+
+const container = document.getElementById('universities');
+    container.textContent = '';
+  const displayUniversity1 = universities => {
+    universities.forEach ( university => {
+  const div = document.createElement("div");
+  div.classList = 'col';
+  div.innerHTML = `
+  <div class="card h-100 university-col">
+              
+              <div class="card-body">
+                <h6 class="card-title uni-color">${university.name}</h6>
+                <p class="card-text country-color">Country: ${university.country}</p>
+                <a class="hero-btn red-btn " id="my-btn text-center" target="_blank" href="${university.web_pages}">Visit</a>
+              </div>
+            </div>
+  `
+  console.log(university)
+  container.appendChild(div)
+  } )
+  // toggole('spinner', 'none')
+  // toggole('uni-section', 'block')
+ }
